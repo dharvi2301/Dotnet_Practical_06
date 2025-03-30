4c20b4b709de6af95f4b3d20b2cfd6b5b66f5a72
dharvi_kaneriya <dharvikaneriya@gmail.com>
2025-03-30 16:24:04 +0530

    feature:Implemented the practical on events.

---
+++ Practical_06/Practical_06/ProcessBusinessLogic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_06
+{
+    // Define a delegate for the event
+    public delegate void ProcessCompletedHandler();
+    public class ProcessBusinessLogic
+    {
+        // Declare the event using the delegate
+        public event ProcessCompletedHandler ProcessCompleted;
+
+        public void StartProcess()
+        {
+            Console.WriteLine("Process Started!");
+
+            // Simulate some work
+            System.Threading.Thread.Sleep(2000);
+
+            // Invoke the event
+            OnProcessCompleted();
+        }
+
+        protected virtual void OnProcessCompleted()
+        {
+            // Check if there are any subscribers, then invoke the event
+            ProcessCompleted?.Invoke();
+        }
+    }
+}
---
+++ Practical_06/Practical_06/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_06
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize the class
+            ProcessBusinessLogic bl = new ProcessBusinessLogic();
+
+            // Subscribe to the event
+            bl.ProcessCompleted += Bl_ProcessCompleted;
+
+            // Start the process
+            bl.StartProcess();
+            Console.ReadLine();
+        }
+        // Event handler method
+        static void Bl_ProcessCompleted()
+        {
+            Console.WriteLine("Method Invoked");
+        }
+    }
+}
